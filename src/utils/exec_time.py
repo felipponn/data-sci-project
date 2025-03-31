b7f6057bec6282582030079264d2935a42ec6f82
Gabriel Pereira <gabrielpereirajp1201@gmail.com>
2025-03-31 18:20:10 -0300

    Modify plot function

--- src/utils/exec_time.py
+++ src/utils/exec_time.py
@@ -51,7 +51,7 @@ def plot_query_times(query_times, search_function):
     plt.xlabel('Query ID')
     plt.ylabel('Execution Time (seconds)')
     plt.title(f'Execution Time per Query for {search_function.__name__}')
-    plt.xticks(rotation=90)
     plt.grid(True)
     plt.tight_layout()
     plt.show()
+    
